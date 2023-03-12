d226f29e19fbd8dba4d22b757223f532d4f511a1
EC2 Default User <ec2-user@ip-172-31-69-219.ec2.internal>
2023-03-12 17:46:50 +0000

    Coding completed 01

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=oebinisa/devops02
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run devops02\
+    --image=$dockerpath\
+    --port=80 --labels app=devops02
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward devops02 8000:80
